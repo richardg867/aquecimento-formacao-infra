4c9e7cfec3b25fa7b6d1955dbc7959bdd7a5681d
RichardG867 <richardg867@gmail.com>
2023-02-27 11:30:56 -0300

    Etapa 2: converter para DataFrame e salvar CSV

--- exercicio.py
+++ exercicio.py
@@ -1,11 +1,12 @@
-import requests
+import pandas, requests
 
 # Etapas cumpridas:
 # 1. obter_dados
+# 2. obter_converter_dados
 
-def obter_dados():
+def obter_dados(n):
 	# Efetuar requisição da API.
-	response = requests.get('https://randomuser.me/api/')
+	response = requests.get('https://randomuser.me/api/?results={0}'.format(n))
 	if response.status_code != 200:
 		print('API retornou status', response.status_code)
 		return None
@@ -18,13 +19,31 @@ def obter_dados():
 
 	return dados
 
-def main():
+def obter_converter_dados(n):
 	# Obter dados da API.
-	dados = obter_dados()
-	if not dados:
+	dados = obter_dados(n)
+	if not dados or 'results' not in dados:
+		return None
+
+	# Normalizar dados para torná-los planos.
+	normalizado = pandas.json_normalize(dados['results'])
+
+	# Converter dados em DataFrame.
+	frame = pandas.DataFrame(normalizado)
+
+	# Salvar dados em CSV.
+	frame.to_csv('dados.csv')
+
+	return frame
+
+
+def main():
+	# Obter e converter dados da API para 1000 usuários.
+	frame = obter_converter_dados(1000)
+	if frame is None:
 		return
 
-	print(dados)
+	print(frame)
 
 if __name__ == '__main__':
 	main()
